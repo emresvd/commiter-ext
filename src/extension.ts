0a16a0684a6dac4e4ac8c2b910585dae90a86813
emresvd <109899306+emresvd@users.noreply.github.com>
2023-08-03 16:56:30 +0300

    modified:   package.json	modified:   src/extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -7,7 +7,13 @@ import * as vscode from 'vscode';
  * @param context The extension context.
  */
 export function activate(context: vscode.ExtensionContext) {
-	let disposable = vscode.commands.registerCommand('commiter-ext.commiter', () => {
+	/**
+	 * Registers the `commiter-ext.commiter` command.
+	 * This command opens a terminal and runs the `commiter` command.
+	 * If a terminal already exists, it uses that terminal, otherwise it creates a new one.
+	 * @param context The extension context.
+	 */
+	let disposable_commiter = vscode.commands.registerCommand('commiter-ext.commiter', () => {
 		if (vscode.window.terminals.length > 0) {
 			const terminal: vscode.Terminal = vscode.window.terminals[0];
 			terminal.sendText("commiter");
@@ -18,7 +24,29 @@ export function activate(context: vscode.ExtensionContext) {
 			terminal.show();
 		}
 	});
-	context.subscriptions.push(disposable);
+	/**
+	 * Registers the `commiter-ext.cgcli` command.
+	 * This command opens a terminal and runs the `cgcli` command.
+	 * If a terminal already exists, it uses that terminal, otherwise it creates a new one.
+	 * @param context The extension context.
+	 */
+	let disposable_cgcli = vscode.commands.registerCommand('commiter-ext.cgcli', () => {
+		if (vscode.window.terminals.length > 0) {
+			const terminal: vscode.Terminal = vscode.window.terminals[0];
+			terminal.sendText("cgcli");
+			terminal.show();
+		} else {
+			const terminal: vscode.Terminal = vscode.window.createTerminal("cgcli");
+			terminal.sendText("cgcli");
+			terminal.show();
+		}
+	});
+	/**
+	 * Adds the `disposable_commiter` and `disposable_cgcli` commands to the extension context's subscriptions.
+	 * @param context The extension context.
+	 */
+	context.subscriptions.push(disposable_commiter);
+	context.subscriptions.push(disposable_cgcli);
 }
 
 /**

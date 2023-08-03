34933c25816684440e5b623e7c9f0d07ae1ab8ee
emresvd <109899306+emresvd@users.noreply.github.com>
2023-08-03 17:26:26 +0300

    modified:   package.json	modified:   src/extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -1,55 +1,44 @@
 import * as vscode from 'vscode';
 
 /**
- * Activates the extension and registers the `commiter-ext.commiter` command.
- * This command opens a terminal and runs the `commiter` command.
- * If a terminal already exists, it uses that terminal, otherwise it creates a new one.
+ * Runs a command in the VS Code terminal. If a terminal already exists, the command is executed in that terminal.
+ * Otherwise, a new terminal is created and the command is executed in that terminal.
+ * @param command The command to execute in the terminal.
+ */
+function runCommand(command: string) {
+	if (vscode.window.terminals.length > 0) {
+		const terminal: vscode.Terminal = vscode.window.terminals[0];
+		terminal.sendText(command);
+		terminal.show();
+	} else {
+		const terminal: vscode.Terminal = vscode.window.createTerminal(command);
+		terminal.sendText(command);
+		terminal.show();
+	}
+}
+
+/**
+ * Activates the extension and registers commands for running the `commiter`, `cgcli`, and `commiter_gui` tools.
  * @param context The extension context.
  */
 export function activate(context: vscode.ExtensionContext) {
-	/**
-	 * Registers the `commiter-ext.commiter` command.
-	 * This command opens a terminal and runs the `commiter` command.
-	 * If a terminal already exists, it uses that terminal, otherwise it creates a new one.
-	 * @param context The extension context.
-	 */
 	let disposable_commiter = vscode.commands.registerCommand('commiter-ext.commiter', () => {
-		if (vscode.window.terminals.length > 0) {
-			const terminal: vscode.Terminal = vscode.window.terminals[0];
-			terminal.sendText("commiter");
-			terminal.show();
-		} else {
-			const terminal: vscode.Terminal = vscode.window.createTerminal("commiter");
-			terminal.sendText("commiter");
-			terminal.show();
-		}
+		runCommand("commiter");
 	});
-	/**
-	 * Registers the `commiter-ext.cgcli` command.
-	 * This command opens a terminal and runs the `cgcli` command.
-	 * If a terminal already exists, it uses that terminal, otherwise it creates a new one.
-	 * @param context The extension context.
-	 */
-	let disposable_cgcli = vscode.commands.registerCommand('commiter-ext.cgcli', () => {
-		if (vscode.window.terminals.length > 0) {
-			const terminal: vscode.Terminal = vscode.window.terminals[0];
-			terminal.sendText("cgcli");
-			terminal.show();
-		} else {
-			const terminal: vscode.Terminal = vscode.window.createTerminal("cgcli");
-			terminal.sendText("cgcli");
-			terminal.show();
-		}
-	});
-	/**
-	 * Adds the `disposable_commiter` and `disposable_cgcli` commands to the extension context's subscriptions.
-	 * @param context The extension context.
-	 */
 	context.subscriptions.push(disposable_commiter);
+
+	let disposable_cgcli = vscode.commands.registerCommand('commiter-ext.cgcli', () => {
+		runCommand("cgcli");
+	});
 	context.subscriptions.push(disposable_cgcli);
+
+	let disposable_commiter_gui = vscode.commands.registerCommand('commiter-ext.commiter_gui', () => {
+		runCommand("commiter_gui .");
+	});
+	context.subscriptions.push(disposable_commiter_gui);
 }
 
 /**
  * Deactivates the extension.
  */
-export function deactivate() {}
+export function deactivate() { }

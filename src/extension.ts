ff112ce0fd2016f2139135f561c3610c53ae659b
emresvd <109899306+emresvd@users.noreply.github.com>
2023-08-03 13:16:08 +0300

    modified:   src/extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -1,54 +1,27 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import { exec } from 'child_process';
 
-let activeTerminal = null;
-
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+/**
+ * Activates the extension and registers the `commiter-ext.commiter` command.
+ * This command opens a terminal and runs the `commiter` command.
+ * If a terminal already exists, it uses that terminal, otherwise it creates a new one.
+ * @param context The extension context.
+ */
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "commiter-ext" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('commiter-ext.commiter', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-
 		if (vscode.window.terminals.length > 0) {
-			const terminal = vscode.window.terminals[0];
+			const terminal: vscode.Terminal = vscode.window.terminals[0];
 			terminal.sendText("commiter");
 			terminal.show();
 		} else {
-			const terminal = vscode.window.createTerminal("Commiter Terminal");
+			const terminal: vscode.Terminal = vscode.window.createTerminal("commiter");
 			terminal.sendText("commiter");
 			terminal.show();
 		}
-
-
-		// const terminal = vscode.window.createTerminal("commiter");
-		// terminal.sendText("commiter");
-		// terminal.show();
-
-		// exec('commiter', { cwd: vscode.workspace.rootPath },
-		// 	(err: any, stdout: any, stderr: any) => {
-		// 		if (err) {
-		// 			console.log(err);
-		// 			vscode.window.showInformationMessage(err.message);
-		// 			return;
-		// 		}
-		// 		console.log(stdout);
-		// 		vscode.window.showInformationMessage(stdout);
-		// 	});
 	});
-
 	context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
+/**
+ * Deactivates the extension.
+ */
 export function deactivate() { }
